e4b8ec2be65d0a588af9a89091cdfddf6dd898d6
Gustavo Silva <gustavosilvafranco643@gmail.com>
2022-03-29 16:04:52 -0300

    chore: auth class modified

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,23 +1,23 @@
 export default class Auth {
-  _token_key: string;
+  tokenKey: string;
 
   constructor(token_key: string) {
-    this._token_key = token_key;
+    this.tokenKey = token_key;
   }
 
   isAuthenticated() {
-    return localStorage.getItem(this._token_key);
+    return localStorage.getItem(this.tokenKey);
   }
 
   getToken() {
-    return localStorage.getItem(this._token_key);
+    return localStorage.getItem(this.tokenKey);
   }
 
   login(token: string) {
-    return localStorage.setItem(this._token_key, token);
+    return localStorage.setItem(this.tokenKey, token);
   }
 
   logout() {
-    return localStorage.removeItem(this._token_key);
+    return localStorage.removeItem(this.tokenKey);
   }
 }
