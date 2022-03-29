fe607136af71a92fa8ed646fd925a9b2f394cdb8
Gustavo Silva <gustavosilvafranco643@gmail.com>
2022-03-29 15:25:41 -0300

    feat: global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,35 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  :root {
+    --primary-color: #AB2680;
+    --font-family-primary: 'Heebo', sans-serif;
+  }
+  
+  body {
+    /* margin/padding */
+    margin: 0;
+    padding: 0;
+
+    /* box */
+    box-sizing: border-box;
+
+    /* font */
+    font-family: var(--font-family-primary);
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-weight: 500;
+  }
+`
+
+export default GlobalStyle;
